5dd13d4fb185fe488c9b2558aa7d858db1cd69ff
ridwanfahmi <ridwanfahmidina12@gmail.com>
2026-04-21 11:10:49 +0700

    menambahkan index.html dan revisi sedikit

--- src/main/java/com/deploy/prak7/controller/UserController.java
+++ src/main/java/com/deploy/prak7/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.deploy.prak7.controller;
 
+import com.deploy.prak7.model.User;
+import com.deploy.prak7.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private  final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "User Berhasil Dibuat";
+    }
+
+    @GetMapping
+    public List<User>getAllUser(){
+        return userService.getAllusers();
+    }
+
+    @DeleteMapping
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return "User Berhasil Dihapus";
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request){
+        userService.updateUser(id, request);
+        return "User Berhasil diupdate";
+    }
 }
