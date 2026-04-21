4e01a589c40c7066747ec880f907286517c89e29
ridwanfahmi <ridwanfahmidina12@gmail.com>
2026-04-21 10:14:52 +0700

    nambahin UserService untuk handle tambah user

---
+++ src/main/java/com/deploy/prak7/service/UserService.java
@@ -0,0 +1,22 @@
+package com.deploy.prak7.service;
+
+import com.deploy.prak7.model.User;
+import com.deploy.prak7.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request){
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+}
