f0122944b9d3b9d160ab9732224c4d16c6f6deb4
ridwanfahmi <ridwanfahmidina12@gmail.com>
2026-04-21 09:38:18 +0700

    Initial commit

---
+++ src/main/java/com/deploy/prak7/Prak7Application.java
@@ -0,0 +1,13 @@
+package com.deploy.prak7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prak7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prak7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/prak7/Prak7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.prak7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prak7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
